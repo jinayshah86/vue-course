8a6f662380397b078cbd8c997a90926755aa7956
Jinay Shah <jinayshah86@gmail.com>
2019-02-24 00:52:36 +0530

    Dynamic Routing & History Mode

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1>List Event</h1>
+  <div>
+    <h1>List Event</h1>
+    <router-link :to="{ name: 'event-show', params: { id: 1 } }"
+      >Show Event #1</router-link
+    >
+  </div>
 </template>
