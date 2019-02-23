a053f4d28bb44a179dca14e6ce6cd4a7237666d9
Jinay Shah <jinayshah86@gmail.com>
2019-02-24 03:58:58 +0530

    State & Getters

--- real-world-vue/src/views/EventCreate.vue
+++ real-world-vue/src/views/EventCreate.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create Event, {{ user.name }}</h1>
+    <p>There are {{ catLength }} categories</p>
+    <ul>
+      <li v-for="category in categories" :key="category">{{ category }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    ...mapState(['user', 'categories'])
+  }
+}
+</script>
+
+<style scoped></style>
