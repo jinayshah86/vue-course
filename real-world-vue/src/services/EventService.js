b3ea5ec878a8c008b8c32a390a3ab067fb069ff7
Jinay Shah <jinayshah86@gmail.com>
2019-02-24 14:18:25 +0530

    Progress Bar: Axios Interceptors

--- real-world-vue/src/services/EventService.js
+++ real-world-vue/src/services/EventService.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import NProgress from 'nprogress'
 
 const apiClient = axios.create({
   baseURL: 'http://localhost:3000',
@@ -7,6 +8,19 @@ const apiClient = axios.create({
     Accept: 'application/json',
     'Content-type': 'application/json'
   }
+})
+
+// Caveat #1 - Not optimal for multiple API calls at the same time
+// Caveat #2 - Templates get rendered before the API call is returned
+
+apiClient.interceptors.request.use(config => {
+  NProgress.start()
+  return config
+})
+
+apiClient.interceptors.response.use(response => {
+  NProgress.done()
+  return response
 })
 
 export default {
